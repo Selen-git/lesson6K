3a11b98026c1252137cf0b22fce8e4d3e391ea7e
Selen-git <Selen_6966@inbox.ru>
2025-03-24 01:13:52 +0500

    Update test_02_calc.py

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,1 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+driver = webdriver.Chrome()
+
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
+delay_input.clear()
+delay_input.send_keys("45")
+
+driver.find_element(By.CSS_SELECTOR, "button#seven").click()
+
+driver.find_element(By.CSS_SELECTOR, "button#add").click()
+
+driver.find_element(By.CSS_SELECTOR, "button#eight").click()
+
+driver.find_element(By.CSS_SELECTOR, "button#equal").click()
+
+result = driver.find_element(By.CSS_SELECTOR, "p#result")
+assert result.text == "15"
